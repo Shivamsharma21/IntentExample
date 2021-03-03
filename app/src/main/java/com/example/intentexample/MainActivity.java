00873c18c55377b19d2e1b3f268f9c1c322f0984
Shivam Sharma <shivam.sharma@berylsystems.com>
2021-03-03 18:16:00 +0530

    Initial commit

---
+++ app/src/main/java/com/example/intentexample/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.intentexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                ///    intents are used to communicate between the activity and also for passing the data
+
+                Intent intent = new Intent(MainActivity.this,SecondActivity.class);
+                startActivity(intent); // this will start the Second Activity
+            }
+        });
+
+    }
+}
